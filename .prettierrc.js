45525bfcb7b9c6e2b2e7b5b64f93591e4c528d53
백혜인 <81304598+hyein0112@users.noreply.github.com>
2024-12-19 01:54:14 +0900

    setting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  arrowParens: "always",
+  // bracketSameLine: true,
+  // bracketSpacing: false,
   singleQuote: true,
-  trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 120,
+  semi: true,
+  trailingComma: "all",
 };
