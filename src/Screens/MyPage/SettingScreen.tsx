f8bab69048eb2ef4cdb5dda234836cf5a5c06dec
백혜인 <81304598+hyein0112@users.noreply.github.com>
2025-01-22 00:10:47 +0900

    feat: 설정 스크린 퍼블리싱

--- src/Screens/MyPage/SettingScreen.tsx
+++ src/Screens/MyPage/SettingScreen.tsx
@@ -1,15 +1,27 @@
 import React, { useLayoutEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Linking, Pressable, StyleSheet, View } from 'react-native';
 import { NavigationHeader, Text, TouchableSVG } from '../../Components/Common';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { ArrowBack } from '../../assets/icons';
+import Color from '../../Common/Color';
+import { FontFamily, FontStyle } from '../../Common/Font';
 
 interface MyPageScreenProps {
   navigation: any;
 }
 
 function MyPageScreen({ navigation }: MyPageScreenProps) {
+  const LinkMenu = [
+    { title: '공지사항', url: 'https://github.com/tododeoji' },
+    { title: '문의하기', url: 'https://github.com/tododeoji' },
+    { title: '이용약관', url: 'https://github.com/tododeoji' },
+    { title: '개인정보처리방침', url: 'https://github.com/tododeoji' },
+    { title: '릴리즈 노트', url: 'https://github.com/tododeoji' },
+    { title: '앱 버전 정보', version: '1.0.0' },
+  ];
+
   const insets = useSafeAreaInsets();
+
   useLayoutEffect(() => {
     navigation.setOptions({
       header: () => (
@@ -24,7 +36,34 @@ function MyPageScreen({ navigation }: MyPageScreenProps) {
 
   return (
     <View style={styles.Container}>
-      <Text>hihi</Text>
+      {LinkMenu.map(({ title, url, version = '' }, index) => (
+        <Pressable
+          key={index + title}
+          onPress={() => url && Linking.openURL(url)}
+          style={[
+            styles.LinkButton,
+            (index + 1) % 2 === 0
+              ? { borderBottomRightRadius: 8, borderBottomLeftRadius: 8, marginBottom: 24 }
+              : { borderTopRightRadius: 8, borderTopLeftRadius: 8, marginBottom: 1 },
+          ]}
+        >
+          <Text>{title}</Text>
+          <Text color={Color.gray3}>{version}</Text>
+        </Pressable>
+      ))}
+
+      <View style={[styles.AccountButton, { bottom: insets.bottom + 32 }]}>
+        <Pressable>
+          <Text fontFamily={FontFamily.REGULAR} fontStyle={FontStyle.caption1} color={Color.gray3}>
+            로그아웃
+          </Text>
+        </Pressable>
+        <Pressable>
+          <Text fontFamily={FontFamily.REGULAR} fontStyle={FontStyle.caption1} color={Color.gray3}>
+            계정 삭제
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -34,6 +73,21 @@ const styles = StyleSheet.create({
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
+    paddingHorizontal: 24,
+  },
+  LinkButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: Color.gray1,
+    width: '100%',
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+  },
+  AccountButton: {
+    flexDirection: 'column',
+    gap: 4,
+    position: 'absolute',
+    left: 40,
   },
 });
 
