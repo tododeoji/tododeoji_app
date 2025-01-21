074eba2fe23193d2f91d707ef9d642923563f553
hyein0112 <s21046@gsm.hs.kr>
2025-01-21 16:30:07 +0900

    feat: 마이 페이지 퍼블리싱

---
+++ src/Components/MyPage/ProFile.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Pressable, StyleSheet, View } from 'react-native';
+import FastImage from 'react-native-fast-image';
+import { Text } from '../Common';
+import { FontFamily, FontStyle } from '../../Common/Font';
+import Color from '../../Common/Color';
+
+function ProFile() {
+  return (
+    <View style={styles.ProfileBox}>
+      <FastImage style={styles.Image} source={require('../../assets/images/yellow_dozy.png')} />
+      <View style={styles.TextBox}>
+        <Text fontFamily={FontFamily.BOLD} fontStyle={FontStyle.description2}>
+          두지
+        </Text>
+        <Text fontStyle={FontStyle.caption1}>email@email.com</Text>
+        <Text fontStyle={FontStyle.description2} style={styles.Bio}>
+          안녕하세요두지예요안녕하세요두지예요안녕하세요두지예요안녕하세요두지예요안녕하세요두지예요안녕하세요
+        </Text>
+        <Pressable style={styles.EditButton}>
+          <Text fontFamily={FontFamily.BOLD} fontStyle={FontStyle.caption2}>
+            프로필 수정
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  ProfileBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 20,
+    paddingVertical: 16,
+  },
+  Image: {
+    width: 104,
+    height: 104,
+    borderRadius: 52,
+  },
+  TextBox: { flexDirection: 'column', alignItems: 'flex-start' },
+  Bio: { maxWidth: '80%', marginVertical: 8 },
+  EditButton: { backgroundColor: Color.gray1, paddingVertical: 4, paddingHorizontal: 12, borderRadius: 20 },
+});
+
+export default ProFile;
--- src/Screens/MyPage/index.tsx
+++ src/Screens/MyPage/index.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Text } from '../../Components/Common';
+import { Pressable, StyleSheet, View } from 'react-native';
+import { H, Text } from '../../Components/Common';
+import Color from '../../Common/Color';
+import ProFile from '../../Components/MyPage/ProFile';
+import { BoxIcon } from '../../assets/icons';
 
 interface MyPageScreenProps {}
 
 function MyPageScreen({}: MyPageScreenProps) {
   return (
-    <SafeAreaView>
-      <Text>MyPage Screen</Text>
-    </SafeAreaView>
+    <View style={styles.Container}>
+      <ProFile />
+      <H h={16} />
+      <Pressable style={styles.SettingButton}>
+        <BoxIcon />
+        <Text>카테고리 관리</Text>
+      </Pressable>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    paddingHorizontal: 24,
+  },
+
+  SettingButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+    width: '100%',
+    padding: 16,
+    borderWidth: 1,
+    borderColor: Color.gray2,
+    borderRadius: 8,
+  },
+});
 
 export default MyPageScreen;
