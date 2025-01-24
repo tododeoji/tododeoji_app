c50df4d5b9ac0988d89df544e296cc143769289a
hyein0112 <s21046@gsm.hs.kr>
2025-01-24 18:37:04 +0900

    feat: splash

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Navigation from './src/Navigation';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 
 interface AppProps {}
 
 function App({}: AppProps) {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1500);
+  }, []);
+
   return (
     <SafeAreaProvider>
       <Navigation />
