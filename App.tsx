8265bfef6931be9565a89c49a400f1809571b564
백혜인 <81304598+hyein0112@users.noreply.github.com>
2025-01-26 23:59:16 +0900

    update

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-import {  SafeAreaProvider  } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import SplashScreen from 'react-native-splash-screen';
 import RootStackNavigator from './src/Navigation/RootStack';
 
